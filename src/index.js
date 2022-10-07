2e9daea9ff42c47f97e3b1df92381311e97f5838
HornyKazuma <me@hornykazuma.ru>
2022-10-07 11:45:40 +0300

    d
    d

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ export default (task, getArrWithAnswerQuestion) => {
   const numberOfAttempts = 3;
   for (let i = 1; i <= numberOfAttempts; i += 1) {
     const getQuestionWithAnswer = getArrWithAnswerQuestion();
+    // На каждой итерации формируем новый вопрос и ответ.
+    // Функция формирования ответа и вопроса у каждой игры своя.
     console.log(`Question: ${getQuestionWithAnswer[0]}`);
     const playerAnswer = getAnswer('Your answer: ');
     if (playerAnswer === getQuestionWithAnswer[1]) {
