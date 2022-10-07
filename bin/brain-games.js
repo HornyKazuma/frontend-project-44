1071a1749ab55c48b4ed299eb97df567e6d15c3b
HornyKazuma <me@hornykazuma.ru>
2022-10-07 11:06:40 +0300

    ss
    ss

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-//import askUserName from '../src/cli.js';
+import askUserName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
-//askUserName();
+askUserName();
