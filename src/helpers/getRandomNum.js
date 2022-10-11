ff48ab6022562db3037fc78fa502daecff63aa21
HornyKazuma <me@hornykazuma.ru>
2022-10-11 16:53:51 +0300

    lint
    lint

--- src/helpers/getRandomNum.js
+++ src/helpers/getRandomNum.js
@@ -1,5 +1,5 @@
 function getRandomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  export default getRandomNum;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default getRandomNum;
