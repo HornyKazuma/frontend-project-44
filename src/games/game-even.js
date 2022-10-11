2ea06d74b857517d382b48bb09e7c24c071b91f2
HornyKazuma <me@hornykazuma.ru>
2022-10-11 15:35:55 +0300

    Update game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,11 +1,12 @@
 import getRandomNum from '../helpers/getRandomNum.js';
-import isEven from '../helpers/isEven.js';
 import gameCore from '../index.js';
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (num) => num % 2 === 0;
+
 const getArrWithAnswerQuestion = () => {
-  const randomNum = getRandomNum();
+  const randomNum = getRandomNum(1, 10);
   const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
   return [randomNum, correctAnswer];
 };
