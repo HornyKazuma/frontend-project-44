5a63aaa4eb1d14ec43ba90adef7df769e1ad79fb
HornyKazuma <me@hornykazuma.ru>
2022-10-11 10:53:30 +0300

    Update getRandomNum.js
    up random

--- src/helpers/getRandomNum.js
+++ src/helpers/getRandomNum.js
@@ -1,5 +1,5 @@
-//export default (index = 10, method = 'ceil') => (method === 'ceil' ? Math.ceil(Math.random() * index) : Math.floor(Math.random() * index));
 export default (int = 100) => Math.floor(Math.random() * int);
+
   
 
   
